ff5d20e5020029a2462ef8708a7026a11792d654
mdrafi276 <mdrafi37473@gmail.com>
2023-10-18 00:10:00 -0700

    footer animation and footer responsive complete

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,5 +1,4 @@
 import Banner from "../../Components/Banner/Banner";
-import Footer from "../../Components/Footer/Footer";
 import Navber from "../../Components/Header/Navber/Navber";
 
 const Home = () => {
@@ -7,7 +6,7 @@ const Home = () => {
       <div className="bg-[#010313]">
         <Navber></Navber>
         <Banner></Banner>
-        <Footer></Footer>
+       
       </div>
     );
 };
